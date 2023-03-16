3c477e27135ed7cb836d54f3dac0d63bf3787cc7
ajin3830 <ajin3830@gmail.com>
2023-03-16 12:39:49 -0400

    complete lab w notes

--- server/app.py
+++ server/app.py
@@ -6,7 +6,7 @@ from flask_migrate import Migrate
 from models import db, Bakery, BakedGood
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db' #direct your Flask app to a database at
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
@@ -20,19 +20,71 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    bakeries = Bakery.query.all()
+    bakeries_serialized = [bakery.to_dict() for bakery in bakeries]
+    # bakeries = [] #WHERE DO I PUT .to_dict() ????
+    # for bakery in Bakery.query.all():
+    #     bakeries.append(bakery)
+    # bakeries_serialized = bakery.to_dict()
+    response = make_response(
+        jsonify(bakeries_serialized),
+        200
+    )
+    # sends the http json header to the browser to 
+    # inform it what kind of data it expects
+
+    # response.headers['Content-Type'] = 'application/json' #optional
+
+    return response
+        # jsonify is a method in Flask that serializes 
+        # (converts from one format to another) the SQLAlchemy object 
+        # (its arguments) as JSON object by getting a list of keys and values 
+        # to pass to the client, then returns a Response object
+        # but due to its limitations we need ?????
+        # SQLAlchemy-serializer in models
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    # bakery = Bakery.query.filter_by(id=id).first()
+    # OR 
+    bakery = Bakery.query.filter(Bakery.id == id).first()
+    bakery_dict = bakery.to_dict()
+    response = make_response(jsonify(bakery_dict), 200)
+
+    # response.headers['Content-Type'] = 'application/json' #optional
+
+    return response
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    # bg_price_desc = BakedGood.query.order_by(BakedGood.price.desc()).first()
+    # baked_goods_dict = [bg_price_desc.to_dict()] 
+    # ^this is wrong even tho it passed the test
+    bg_price_desc = BakedGood.query.order_by(BakedGood.price.desc()).all() 
+    baked_goods_dict = [bg.to_dict() for bg in bg_price_desc] 
+    # OR
+    # price_desc = BakedGood.query.order_by(BakedGood.price).all()
+    # baked_goods_dict = [
+    #     bg.to_dict() for bg in price_desc
+    # ]
+    response = make_response(jsonify(baked_goods_dict), 200)
+
+    # response.headers['Content-Type'] = 'application/json' #optional
+
+    return response
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    # most_expensive = BakedGood.query.order_by(BakedGood.price.desc()).limit(1).first()
+    # ^this is wrong, limit() is used in vanila sql, here we use first()  
+
+    most_expensive = BakedGood.query.order_by(BakedGood.price.desc()).first() 
+    baked_goods_dict = most_expensive.to_dict()
+    response = make_response(jsonify(baked_goods_dict), 200)
+
+    # response.headers['Content-Type'] = 'application/json' #optional
+
+    return response
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -5,11 +5,40 @@ from sqlalchemy_serializer import SerializerMixin
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
 })
-
 db = SQLAlchemy(metadata=metadata)
 
-class Bakery(db.Model, SerializerMixin):
-    pass
+# db = SQLAlchemy()
 
+class Bakery(db.Model, SerializerMixin):
+    __tablename__ = 'bakeries'
+
+    # this help add baked_goods to the bakery; Tuple, add comma if single!!
+    # so baked_goods from the bakery can be attached to the request
+    serialize_rules = ('-baked_goods.bakery',)
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    baked_goods = db.relationship('BakedGood', backref='bakery')
+
+    def __repr__(self):
+        return f'<Bakery: {self.name}>' 
 class BakedGood(db.Model, SerializerMixin):
-    pass
+    __tablename__ = 'baked_goods'
+
+    # this help add bakery to the response
+    serialize_rules = ('-bakery.baked_goods',)
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True)
+    price = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())    
+
+    # pluralize bakery here!! Bc it's referencing the bakeries table up top!!
+    bakery_id = db.Column(db.Integer, db.ForeignKey('bakeries.id'))
+
+    def __repr__(self):
+        return f'<BakedGood: {self.name}, Price: {self.price}>'
